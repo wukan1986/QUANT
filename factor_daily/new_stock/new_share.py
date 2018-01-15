a32dc815c1ade6279b1020521ceefd0c55a1ea1f
shiyunchao <shiyunchao@outlook.com>
2018-01-15 10:08:05 +0800

    对不同生产账户 生成新股

--- factor_daily/new_stock/new_share.py
+++ factor_daily/new_stock/new_share.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from datetime import datetime, timedelta
 import sys
+import yaml
 from time import strftime, localtime
 
 sys.path.append("F:/QUANT/")
@@ -20,23 +21,30 @@ def get_new_stock(day, account_name, output_path, get_db):
     newshares['value'] = 1
     newshares['S_INFO_CODE'] = newshares['S_INFO_CODE'].apply(lambda x: x + '-CN')
 
-    df = pd.DataFrame([['', 'GROUP'], ['name', account_name], ['unit', 'number'], ['', '']],
-                      columns=['S_INFO_CODE', 'value'])
-    df = pd.concat([df, newshares])
+    # df = pd.DataFrame([['', 'GROUP'], ['name', account_name], ['unit', 'number'], ['', '']],
+    #                   columns=['S_INFO_CODE', 'value'])
+    # df = pd.concat([df, newshares])
 
-    df.to_csv('%s/%s' % (output_path, 'no_sell_99991231.csv'), index=None, header=None)
+    newshares.to_csv('%s/%s' % (output_path, 'nosell_%s_99991231.csv'%account_name), index=None, header=None)
 
 
-def run(start, end, today, account_name, dirpath, output_path, flag, fre):
-    getdb = read_db(type='wind')
-    tradeday = get_tradeDay.wind(start, end, fre=fre)
-    tradeday = tradeday.iloc[:-1]
-    if flag == 0:
-        for day in tradeday:
-            print(day)
-            get_new_stock(day, account_name, output_path, getdb)
-    else:
-        get_new_stock(today, account_name, output_path, getdb)
+def run(start, end, today, output_path, flag, fre):
+    config_path = 'Z:/daily_data/config/config.yaml'
+    with open(config_path) as f:
+        config = yaml.load(f.read())
+    account = config['account']
+    for key, sub_account in account.items():
+        if sub_account['type'] == 'p':
+            account_name = sub_account['subaccount'].split('account-')[1]
+            getdb = read_db(type='wind')
+            tradeday = get_tradeDay.wind(start, end, fre=fre)
+            tradeday = tradeday.iloc[:-1]
+            if flag == 0:
+                for day in tradeday:
+                    print(day)
+                    get_new_stock(day, account_name, output_path, getdb)
+            else:
+                get_new_stock(today, account_name, output_path, getdb)
 
 
 if __name__ == '__main__':
@@ -44,10 +52,8 @@ if __name__ == '__main__':
     start = '20171130'
     end = strftime("%Y%m%d", localtime())
     today = strftime("%Y%m%d", localtime())
-    dirpath = 'G:/ESG100Enhanced/FA'
     output_path = 'Z:/daily_data/no_sell'
     fre = 'day'
-    account_name = 'no_sell_ESG100-pdt'
     flag = 1
-    run(start, end, today, account_name, dirpath, output_path, flag, fre)
+    run(start, end, today, output_path, flag, fre)
     print('Done!')
